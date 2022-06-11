46577e3eb1016341e8b04d7d3903c6fcd200c9a7
Ahmad-Arabi <ahmadarabi2562@gmail.com>
2022-06-11 22:06:54 +0600

    APWB_Assignment_2 Added

---
+++ APWT_SUM22_Assign/Assign2_demo/resources/views/layout/main2.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head></head>
+    <body>
+        <div>
+            <a href="{{route('login')}}">Login</a>
+            <a href="{{route('register')}}">Registration</a>
+            <br><br>
+        </div>
+        @yield('content')
+    </body>
+</html>
